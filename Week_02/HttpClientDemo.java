dc9b934b4ba9ef8fba8e5807ef42b992ac4d1c0c
Allen Chen <as_allen1128@hotmail.com>
2020-10-28 23:56:39 +0800

    2nd week

---
+++ Week_02/HttpClientDemo.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+public class HttpClientDemo {
+    public static void main(String[] args) throws IOException {
+        String url = String.format("http://localhost:8801");
+        HttpGet httpGet = new HttpGet(url);
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+            System.out.println(EntityUtils.toString(httpResponse.getEntity()));
+        }
+    }
+}
